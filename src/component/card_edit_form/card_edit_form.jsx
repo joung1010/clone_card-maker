470d94cbae9fa6d8e39108e18d7a9ef4713f7361
mason park <joung4342@gmail.com>
2022-06-30 21:14:42 +0900

    add edit component

--- src/component/card_edit_form/card_edit_form.jsx
+++ src/component/card_edit_form/card_edit_form.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import styles from './card_edit_form.module.css';
+import Button from "../../common/button/button";
+import ImageFileInput from "../../common/img_file_input/imageFileInput";
 
-const CardEditForm = ({card}) => (
-    <h1>{card.id}</h1>
-);
+const CardEditForm = ({card}) => {
+    const {name,company,theme,title,email,message,fileName,fileURL} = card;
+    const onSubmit = () => {
+
+    };
+    return(
+        <form className={styles.form}>
+            <input className={styles.input} type="text" name="name" value={name}/>
+            <input className={styles.input} type="text" name="company" value={company}/>
+            <select className={styles.select} name="theme" value={theme}>
+                <option value="light">Light</option>
+                <option value="Dark">dark</option>
+                <option value="colorful">Colorful</option>
+            </select>
+            <input className={styles.input} type="text" name="title" value={title}/>
+            <input className={styles.input} type="text" name="email" value={email}/>
+            <textarea className={styles.textarea} name="message" value={message}/>
+            <div className={styles.fileInput}>
+                <ImageFileInput/>
+            </div>
+            <Button name="Delete"
+                onClick={onSubmit}
+            />
+        </form>
+    );
+}
 
 export default CardEditForm;
